6f84179735523c590f3be043744983432f3aac24
jihwan <jihwan1535@nate.com>
2024-05-29 13:47:31 +0900

    refactor(#9) : request, response filter에서 관리

---
+++ src/main/java/com/blog/tech/global/config/ServletFilter.java
@@ -0,0 +1,30 @@
+package com.blog.tech.global.config;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter(filterName = "servletFilter", urlPatterns = "/*")
+public class ServletFilter implements Filter {
+	@Override
+	public void doFilter(
+		final ServletRequest servletRequest,
+		final ServletResponse servletResponse,
+		final FilterChain filterChain
+	) throws IOException, ServletException {
+		HttpServletRequest rq = (HttpServletRequest) servletRequest;
+		HttpServletResponse resp = (HttpServletResponse) servletResponse;
+		rq.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+
+		filterChain.doFilter(rq, resp);
+	}
+}
